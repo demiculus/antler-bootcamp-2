8f062352247073d002959887986c633be6705c12
demiculus <demirbyilmaz@gmail.com>
2020-10-22 09:25:00 +0300

    Updates

---
+++ precache-manifest.5fd04eb4d577fa99a554f4073afa2137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401f5e2da8e39281328be56f3cafd8f0",
+    "url": "/antler-bootcamp-2/index.html"
+  },
+  {
+    "revision": "e476ab49f9f238436905",
+    "url": "/antler-bootcamp-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d91927c993022483ae2b",
+    "url": "/antler-bootcamp-2/static/js/2.824717fc.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/antler-bootcamp-2/static/js/2.824717fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e476ab49f9f238436905",
+    "url": "/antler-bootcamp-2/static/js/main.6f7a12b8.chunk.js"
+  },
+  {
+    "revision": "43d9ccc58d3e45b0e24c",
+    "url": "/antler-bootcamp-2/static/js/runtime-main.5c8aea5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/antler-bootcamp-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
